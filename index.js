753223464495fc82241f5eb0771f065db3f6945d
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-01-25 21:44:05 -0500

    changed a bunch of stuff

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Intern = require('./lib/intern')
 const fs = require('fs');
 const Employee = require('./lib/employee');
 const teamArray = [];
+const createHtml = require('./src/template1')
 
 function runApp() {
     createTeam();
@@ -18,22 +19,16 @@ function runApp() {
         }]).then(function (input) {
             switch (input.addEmployee) {
                 case "Manager":
-                    // Employee.gerneralQuestions();
-                    // manager.gerneralQuestions();
                     createManager();
                     break;
                 case "Engineer":
-                    // Employee.gerneralQuestions();
-                    // engineer.gerneralQuestions();
                     createEngineer();
                     break;
                 case "Intern":
-                    // Employee.gerneralQuestions();
-                    // intern.gerneralQuestions();
                     createIntern();
                     break;
                 default:
-                    createHtml(teamArray);
+                    fs.writeFileSync('./dist/team.html', createHtml(teamArray));;
             }
         })
     }
@@ -98,87 +93,36 @@ function runApp() {
             {
                 type: 'input',
                 message: "What is your Intern's Name: ?",
-                name: "name"
+                name: "internName"
             },
             {
                 type: 'input',
                 message: "What is your Intern's ID number? ",
-                name: "id"
+                name: "internId"
             },
             {
                 type: 'input',
                 message: "What is your Intern's ID Email? ",
-                name: "email"
+                name: "InternEmail"
             },
             {
                 type: 'input',
                 message: "What is your Intern's school? ",
-                name: "school"
+                name: "internSchool"
             },
 
         ]).then(answers => {
             // console.log("this intern function")
             // console.log(answers);
-            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            const intern = new Intern(answers.internName, answers.internId, answers.InternEmail, answers.internSchool);
             // console.log("the variable intern", intern);
             teamArray.push(intern);
             createTeam(teamArray);
         });
     }
-
-
 }
 //this is the end of the function. The main function
-function createHtml(teamArray) {
-    var myHtml = `
-    <!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="Description" content="Enter your description here" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-
-  <title>Team Profile Generator</title>
-</head>
-
-<body>
-  <!-- Page margins are created using the container and container-fluid classes -->
-  <header class="container-fluid bg-danger text-light text-center mb-5 p-3">
-    <div class="d-flex align-items-center">
-      <h1 class="header text-center">My Team</h1>
-    </div>
-  </header>
-<section class="container-sm">
-  <div class="card" style="width: 18rem;">
-    <div class="card-header">
-      Featured
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-      <li class="list-group-item">Vestibulum at eros</li>
-    </ul>
-  </div>
-    <div class="card" style="width: 18rem;">
-      <div class="card-header">
-       ${manager.name}
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
-    </div>
-  </section>
-</body>
-</html>
-    `
-    console.log(teamArray);
-    fs.writeFile('team.html', myHtml);
-}
 //the actually initialization of the function runApp
 runApp();
 
--- src/template1.js
+++ src/template1.js
@@ -1,44 +1,88 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="Description" content="Enter your description here" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-
-  <title>Team Profile Generator</title>
-</head>
-
-<body>
-  <!-- Page margins are created using the container and container-fluid classes -->
-  <header class="container-fluid bg-danger text-light text-center mb-5 p-3">
-    <div class="d-flex align-items-center">
-      <h1 class="header text-center">My Team</h1>
-    </div>
-  </header>
-<section class="container-sm">
-  <div class="card" style="width: 18rem;">
-    <div class="card-header">
-      Featured
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-      <li class="list-group-item">Vestibulum at eros</li>
-    </ul>
-  </div>
-    <div class="card" style="width: 18rem;">
-      <div class="card-header">
-        Featured
+function createHtml(teamArray) {
+   return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  </head>
+  <body>
+      <div class="container-fluid">
+              <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+                  <h1 class="text-center text-white">My Team</h1>
+          </div>
+          <div class="d-flex justify-content-around">
+      ${createTeam(teamArray)}
       </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
-    </div>
-  </section>
+  </div>
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous">
+    </script>
 </body>
 </html>
+      `
+};
+function createTeam(teamArray) {
+let html = "";
+for (var i = 0; i < teamArray.length; i++) {
+  if (teamArray[i].getRole() === "Manager") {
+    html += `
+   <div class="card manager-card w-25 p-5">
+     <div class="card-header bg-primary text-white">
+         <h2 class="card-title" id="manager">${teamArray[i].getName()}</h2>
+         <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
+     </div>
+     <div class="card-body">
+         <ul class="list-group">
+             <li class="list-group-item">ID: ${teamArray[i].getId()}</li>
+             <li class="list-group-item">Email: <a href="mailto:${teamArray[i].getEmail()}">${teamArray[i].getEmail()}</a></li>
+             <li class="list-group-item">Office number: ${teamArray[i].getOfficeNumber()}</li>
+         </ul>
+     </div>
+   </div>
+     `
+  } else if (teamArray[i].getRole() === "Engineer") {
+  html += `
+  <div class="card engineer-card w-25 p-5">
+  <div class="card-header bg-primary text-white">
+      <h2 class="card-title">${teamArray[i].name}</h2>
+      <h3 class="card-title" id="engineer"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+  </div>
+  <div class="card-body">
+      <ul class="list-group">
+          <li class="list-group-item">ID: ${teamArray[i].id}</li>
+          <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+          <li class="list-group-item">GitHub Link: <a href="(https://github.com/${teamArray[i].github})"></a></li>
+      </ul>
+  </div>
+</div>
+  `
+  } else {
+    html += `
+    <div class="card intern-card w-25 p-5">
+  <div class="card-header bg-primary text-white">
+      <h2 class="card-title">${teamArray[i].name}</h2>
+      <h3 class="card-title" id="intern"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
+  </div>
+  <div class="card-body">
+      <ul class="list-group">
+          <li class="list-group-item">ID: ${teamArray[i].id}</li>
+          <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
+          <li class="list-group-item">School: ${teamArray[i].school}</li>
+      </ul>
+  </div>
+  </div>
+    `
+  }
+}
+return html;
+}
+
+module.exports = createHtml
